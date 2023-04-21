67a4f045d7d4bc545a4f7380466d10a4ce2813dc
amimejia <122493428+amimejia@users.noreply.github.com>
2023-04-21 16:34:23 -0400

    Second Commit

--- src/main/java/edu/guilford/userPassword.java
+++ src/main/java/edu/guilford/userPassword.java
@@ -10,9 +10,56 @@ public class userPassword
 {
     public static void main( String[] args )
     {
-        // Ask the user for their favorite holiday
-        System.out.println( ": " );
-        //Declare a Scanner to read from the keyboard 
-        Scanner keyboard = new Scanner(System.in);
-    }
+        //insatiate a new user object
+        user userMe = new user();
+        //create a scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Ask the user for their first name and print it out
+        System.out.println("What is your first name?: ");
+        String firstname = input.nextLine();
+        userMe.setFirstname(firstname);
+        System.out.println("Your first name is: " + userMe.getFirstname());
+
+        // Ask the user for their last name and print it out
+        System.out.println("What is your last name?: ");
+        String lastname = input.nextLine();
+        userMe.setLastname(lastname);
+        System.out.println("Your last name is: " + userMe.getLastname());
+
+        // Ask the user for holiday and print it out
+        System.out.println("What is your favorite holiday?: ");
+        String favholiday = input.nextLine();
+        userMe.setFavholiday(favholiday);
+        System.out.println("Your favorite holiday is: " + userMe.getFavholiday());
+
+        // Ask the user for color and print it out
+        System.out.println("What is your favorite color?: ");
+        String favcolor = input.nextLine();
+        userMe.setFavcolor(favcolor);
+        System.out.println("Your favorite color is: " + userMe.getFavcolor());
+
+        // Ask the user for animal and print it out
+        System.out.println("What is your favorite animal?: ");
+        String favanimal = input.nextLine();
+        userMe.setFavanimal(favanimal);
+        System.out.println("Your favorite animal is: " + userMe.getFavanimal());
+
+        // Ask the user for season and print it out
+        System.out.println("What is your favorite season?: ");
+        String favseason = input.nextLine();
+        userMe.setFavseason(favseason);
+        System.out.println("Your favorite season is: " + userMe.getFavseason());
+
+        //generate a password for the user based on their first name, last name, favorite holiday, favorite color, favorite animal, and favorite season
+        userMe.generatePassword();
+        
+        //encrypt the password using the AES class
+        AES aes = new AES();
+
+        String encryptedPassword = aes.encrypt(userMe.getPassword());
+        System.out.println("Your encrypted password is: " + encryptedPassword);
+
+
+
 }
