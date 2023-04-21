91f7d65cdf60ab78440e74000681d0afb38a4879
amimejia <122493428+amimejia@users.noreply.github.com>
2023-04-21 12:36:01 -0400

    First Commit

---
+++ src/main/java/edu/guilford/user.java
@@ -0,0 +1,22 @@
+package edu.guilford;
+
+public class user {
+    //attributes
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private int password;
+    private String holiday;
+
+    //constructor
+    public user(String username, String firstname, String lastname, String email, int password) {
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    //
+}
---
+++ src/main/java/edu/guilford/userPassword.java
@@ -0,0 +1,18 @@
+package edu.guilford;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class userPassword 
+{
+    public static void main( String[] args )
+    {
+        // Ask the user for their favorite holiday
+        System.out.println( ": " );
+        //Declare a Scanner to read from the keyboard 
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
